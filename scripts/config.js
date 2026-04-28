1e39a4c301b7eb584a44cad76287a6c29c3886a1
jesperhammer1980-jpg <jesperhammer1980@gmail.com>
2026-04-28 18:51:02 +0200

    Create config.js

---
+++ scripts/config.js
@@ -0,0 +1,14 @@
+export const TOMTOM_API_KEY =
+  "sNGuo0IoYgQfVtcno4iGPi3PYQGlhQqP";
+
+export const TOMTOM_ROUTING_BASE =
+  "https://api.tomtom.com/routing/1";
+
+export const TOMTOM_TRAFFIC_BASE =
+  "https://api.tomtom.com/traffic/services/4";
+
+export const TOMTOM_SEARCH_BASE =
+  "https://api.tomtom.com/search/2";
+
+export const TOMTOM_SNAP_BASE =
+  "https://api.tomtom.com/snapToRoads/1";
